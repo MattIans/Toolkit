ac0210239543144ac3adfec666f4552bc552dd5c
Matt Ian <miauss@pm.me>
2021-06-30 10:09:22 +1000

    Add project files.

---
+++ Toolkit.Mobile/Toolkit.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Toolkit.Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace Toolkit.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
